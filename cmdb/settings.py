43d281b4421f45ee0389e7ab68f19dbcd8f72399
lizhong24 <lang812>
2018-05-22 21:22:49 +0800

    资产管理

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*^gkyx5k*2%lvzw*p=o*%84g7tjpswti2=j%tbd7&!ov$d-1ez'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,3 +120,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
